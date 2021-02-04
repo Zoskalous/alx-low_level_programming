f67c44944548ee5cd71a43b85e6471ee189375d2
Zoskalous <zekariyasfekadu54@gmail.com>
2021-02-04 16:05:55 +0000

    C1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <time.h>
+
+/**
+* main -Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char abc = 'a';
+while(abc<='z')
+{
+putchar(abc);
+putchar('\n');
+abc++;
+}
+return (0);
+}
