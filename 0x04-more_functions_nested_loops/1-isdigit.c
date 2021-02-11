1e619cd5ed1d0df8a219bf2ef96d7b2d2bc796bd
Zekariyas Fekadu <78035903+Zoskalous@users.noreply.github.com>
2021-02-11 12:57:28 -0800

    Create 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - check the code
+ *
+ * @c: is an integer param
+ *
+ * Return: Always 0.
+ */
+
+
+int _isdigit(int c)
+{
+return (c >= 48 && c <= 57);
+}
