ea1c990158730530f78564e4b373d51adf4567bc
Zekariyas Fekadu <78035903+Zoskalous@users.noreply.github.com>
2021-02-11 12:58:09 -0800

    Create 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * mul - check the code
+ *
+ * @a: is an integer param
+ *
+ * @b: is an integer param
+ *
+ * Return: Always 0.
+ */
+
+
+int mul(int a, int b)
+{
+
+	return (a * b);
+}
