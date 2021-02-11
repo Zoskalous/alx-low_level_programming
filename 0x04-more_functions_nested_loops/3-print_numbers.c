db515a44891f606a2315c6430ffb69526a8db0c6
Zekariyas Fekadu <78035903+Zoskalous@users.noreply.github.com>
2021-02-11 12:58:43 -0800

    Create 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - check the code
+ *
+ *
+ * Return: No.
+ */
+
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
